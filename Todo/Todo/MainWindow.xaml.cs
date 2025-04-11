cdc19cc12009358f2836a23c043f9eb869bac476
Ali Mansoor <s2306420@student.hb.se>
2025-04-11 15:33:49 +0200

    UPD

--- Todo/Todo/MainWindow.xaml.cs
+++ Todo/Todo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Xml;
 
 
 namespace Todo
@@ -19,18 +20,49 @@ namespace Todo
         private void addButton_Click_1(object sender, RoutedEventArgs e)
         {
 
+            if (string.IsNullOrWhiteSpace(inputTextBox.Text)) 
+            {
+                MessageBox.Show(
+                    "Du kan inte ha tom text. \nVänligen skriv in text i rutan.",
+                    "Felmeddelande",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
             todoList.Add(inputTextBox.Text);
             todoListBox.Items.Refresh();
             inputTextBox.Clear();
-
         }
 
         private void removeButton_Click(object sender, RoutedEventArgs e)
         {
+            if (todoListBox.SelectedItem == null) 
+            {
+                MessageBox.Show(
+                    "Du behöver välja ett alternativ.", "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+                    return;
+            }
+
+
             if (todoListBox.SelectedItem != null)
             {
-                todoList.Remove(todoListBox.SelectedItem.ToString());
-                todoListBox.Items.Refresh();
+                MessageBoxResult resultat = MessageBox.Show(
+                    "Är du säker på att du vill ta bort detta", "Varning",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning);
+
+                if (resultat == MessageBoxResult.OK)
+                {
+                    todoList.Remove(todoListBox.SelectedItem.ToString());
+                    todoListBox.Items.Refresh();
+                }
+                else
+                {
+                    return;
+                }
             }
         }
     }
