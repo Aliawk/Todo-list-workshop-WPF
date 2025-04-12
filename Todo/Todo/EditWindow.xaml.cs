45bf89377f9584245d47c7202001a5f8f76b17a4
Aliawk <alimansoor.jobb@gmail.com>
2025-04-12 13:43:07 +0200

    UPD

---
+++ Todo/Todo/EditWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Todo
+{
+    public partial class EditWindow : Window
+    {
+        public string UpdatedText { get; private set; }
+
+        public EditWindow(string currentText)
+        {
+            InitializeComponent();
+            editTextBox.Text = currentText;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdatedText = editTextBox.Text;
+            DialogResult = true; // Stänger fönstret och signalerar att vi sparat
+        }
+    }
+}
